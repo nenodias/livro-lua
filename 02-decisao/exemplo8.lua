bf8059948dffc70c1442b4b0554ef6f49cf7db42
Horácio Dias <horacio.dias92@gmail.com>
2018-07-15 20:39:32 -0300

    Exemplos usando o operador de módulo

---
+++ 02-decisao/exemplo8.lua
@@ -0,0 +1,16 @@
+--programa cap311.lua
+
+io.write("Digite um valor numérico: ")
+N = tonumber(io.read())
+
+R4 = N % 4
+R5 = N % 5
+
+if (R4 == 0) and (R5 == 0) then
+    print(N)
+else
+    print("Valor não é divisível por 4 e 5.")
+end
+
+io.write("Tecle <Enter> para encerrar..")
+io.read('*l')
